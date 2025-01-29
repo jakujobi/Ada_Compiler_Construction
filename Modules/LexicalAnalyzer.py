55939aabfd8f025376a6ddebdcecb5ce9fdec776
John <john@jakujobi.com>
2025-01-29 00:29:34 -0600

    Add initial implementation of JohnA1.py, Token.py, and update LexicalAnalyzer.py to include necessary imports and path adjustments for module accessibility

--- Modules/LexicalAnalyzer.py
+++ Modules/LexicalAnalyzer.py
@@ -1,2 +1,15 @@
 # LexicalAnalyzer
 # LexicalAnalyzer is a module that provides a lexical analyzer for Ada programs.
+
+
+
+import os
+import sys
+from typing import List, Optional
+
+from pathlib import Path
+
+repo_home_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(repo_home_path)
+
+from Modules.FileHandler import FileHandler
---
+++ Modules/Token.py
@@ -0,0 +1,16 @@
+# Token.py
+
+
+import os
+import sys
+from typing import List, Optional
+
+from pathlib import Path
+
+repo_home_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(repo_home_path)
+
+#from Modules.
+
+
+
