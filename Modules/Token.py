eb809236594b2930c0de846e34a1e6e3e5406b22
John <john@jakujobi.com>
2025-01-31 00:24:51 -0600

    Add Token class to manage token attributes and improve representation

--- Modules/Token.py
+++ Modules/Token.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import logging
 from typing import List, Optional
 
 from pathlib import Path
@@ -14,3 +15,16 @@ sys.path.append(repo_home_path)
 
 
 
+class Token:
+    def __init__(self, type, lexeme, line_number, column_number, value=None):
+        self.type = type  # Instance of Definitions.TokenType
+        self.lexeme = lexeme
+        self.line_number = line_number
+        self.column_number = column_number
+        self.value = value  # For NUM, REAL, LITERAL tokens
+
+    def __repr__(self):
+        try:
+            return f"Token(type={self.type}, lexeme='{self.lexeme}', value={self.value}, line={self.line_number}, column={self.column_number})"
+        except:
+            logging.error('%s raised an exception', self)
