a2a07c122fbf7a89713cc0256191633da4392f23
John <john@jakujobi.com>
2025-02-01 23:23:45 -0600

    Add LexicalAnalyzer class with file and error handler integration

--- Modules/LexicalAnalyzer.py
+++ Modules/LexicalAnalyzer.py
@@ -5,6 +5,7 @@
 
 import os
 import sys
+import logging
 from typing import List, Optional
 
 from pathlib import Path
@@ -15,3 +16,24 @@ sys.path.append(repo_home_path)
 from Modules.FileHandler import FileHandler
 from Modules.Token import Token
 from Modules.Definitions import Definitions
+from Modules.ErrorHandler import ErrorHandler
+
+
+class LexicalAnalyzer:
+    def __init__(self,
+                 file_handler: Optional[FileHandler] = None,
+                 error_handler: Optional[ErrorHandler] = None
+                 ):
+        self.file_handler = file_handler or FileHandler()
+        self.error_handler = error_handler or ErrorHandler()
+        self.pos = 0
+        self.current_char = ''
+        self.line = 1
+        self.column = 0
+        #self._read_next_char()  # Initialize the first character
+
+    def get_char(self):
+        pass
+
+    def analyze(self, source_code_string: str) -> List[Token]:
+        pass
